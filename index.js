29ba527e9312a51e80642d0803accaaa31e966cc
prdn <paolo.ardoino@gmail.com>
2018-06-10 08:39:51 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const FLAGS = {
+  ORDER: {
+    'IS_HIDDEN': 64,
+    'IS_POS_CLOSE': 512,
+    'IS_POS_REDUCE': 1024,
+    'IS_POST_ONLY': 4096,
+    'IS_OCO': 16384,
+    'AVOID_FUNDING_VIR': 524288
+  },
+  OFFER: {
+    'IS_HIDDEN': 64
+  }
+}
+
+function has_flag (t, f, c) {
+  return ((f & FLAGS[t][c]) === FLAGS[t][c])
+}
+
+module.exports = {
+  FLAGS: FLAGS,
+  has_flag: has_flag
+}
